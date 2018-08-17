e1d401fadda47717af7a97a6dcc203de55fa8bb2
Joshua Morris <jmorris@clchomeoffice.com>
2018-08-17 15:25:18 -0700

    Update dev server options to make it easier on the developer

--- server.js
+++ server.js
@@ -2,17 +2,24 @@
 
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
+const opn = require('opn');
 
 const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
+    compress: true,
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    open: true,
+    openPage: '/examples',
 }).listen(3000, 'localhost', function(err, result) {
-  if (err) {
-    console.log(err);
-  }
+    if (err) {
+        console.log(err);
+    }
 
-  console.log('Listening at localhost:3000');
+    console.log('Result: ', result);
+
+    console.log('Listening at localhost:3000');
+    opn('http://localhost:3000/examples/');
 });
