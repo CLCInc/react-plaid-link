e376d32bdc69aa215725bc4a05f9deeda0b290d2
Joshua Morris <jmorris@clchomeoffice.com>
2018-08-17 15:24:43 -0700

    Update webpack configs to eslint style and update options

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,47 @@
-var fs = require('fs');
-var path = require('path');
-var webpack = require('webpack');
-
-
-var EXAMPLES_DIR = path.resolve(__dirname, 'examples');
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const EXAMPLES_DIR = path.resolve(__dirname, 'examples');
 
 function isDirectory(dir) {
-  return fs.lstatSync(dir).isDirectory();
+    return fs.lstatSync(dir).isDirectory();
 }
 
 function buildEntries() {
-  return fs.readdirSync(EXAMPLES_DIR).reduce(function (entries, dir) {
-    if (dir === 'build')
-      return entries;
+    return fs.readdirSync(EXAMPLES_DIR).reduce(function(entries, dir) {
+        if (dir === 'build') {return entries;}
 
-    var isDraft = dir.charAt(0) === '_';
+        const isDraft = dir.charAt(0) === '_';
 
-    if (!isDraft && isDirectory(path.join(EXAMPLES_DIR, dir)))
-      entries[dir] = path.join(EXAMPLES_DIR, dir, 'app.js');
+        if (!isDraft && isDirectory(path.join(EXAMPLES_DIR, dir))) {entries[dir] = path.join(EXAMPLES_DIR, dir, 'app.js');}
 
-    return entries;
-  }, {});
+        return entries;
+    }, {});
 }
 
 module.exports = {
-  entry: buildEntries(),
+    mode: 'production',
+    entry: buildEntries(),
 
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/static/',
-  },
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/static/',
+    },
 
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
-    ]
-  },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' }},
+        ],
+    },
 
-  resolve: {
-    alias: {
-      'react-plaid-link': path.resolve(__dirname, './src')
-    }
-  },
+    resolve: {
+        alias: {
+            'react-plaid-link': path.resolve(__dirname, './src'),
+        },
+    },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 };
